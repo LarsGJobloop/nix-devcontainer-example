1168601d784c8227afc93b53c08e53d66e578be6
zabronax <zabronax@gmail.com>
2025-08-06 12:32:04 +0200

    setup environment

---
+++ README.md
@@ -0,0 +1,16 @@
+# GitOps Introduction
+
+GitOps is the practice of using Git and code as the source of all configuration.
+
+## Tooling Overview
+
+- [Nix](https://nixos.org/): Declares which CLI tools and versions that are used in this project
+- [Terraform](https://developer.hashicorp.com/terraform): A commonly used tooling for provisioning and configuring external services
+- [OpenTofu](https://opentofu.org/): Alternative open source variant of Terraform, currently they are largely identical
+- [SOPS](https://github.com/getsops/sops): Tool for managing secrets as part of a version controlled repository
+- [Age](https://github.com/FiloSottile/age): Modern tool for creating cryptographic keys, encrypting and decrypting encoded material
+
+## External Services
+
+- [GitHub](https://github.com/): A cloud provider delivering Version Control Systems, Collaboration platform, Identities, automated workers and binary storage repositories
+- [Hetzner](https://www.hetzner.com/): A cheap, European, cloud provider delivering generic Compute, Network, and Storage Solutions
---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      # Supported systems
+      systems = [
+        "x86_64-linux" # !TODO: Verify
+        "aarch64-linux" # !TODO: Verify
+        "x86_64-darwin" # !TODO: Verify
+        "aarch64-darwin" # Verified
+      ];
+
+      # Helper functions
+      withSystem = nixpkgs.lib.genAttrs systems;
+      withPkgs =
+        callback:
+        withSystem (
+          system:
+          callback (
+            import nixpkgs {
+              config.allowUnfree = true; # Terraform is licensed bsl11
+              inherit system;
+            }
+          )
+        );
+    in
+    {
+      # Development environments
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Infrastructure tools
+            terraform # Commonly used
+            opentofu # Open source alternative to terraform
+
+            # Development Tools
+            openssh # SSH client
+            sops # Secrets management
+            age # Encryption tool
+          ];
+        };
+      });
+    };
+}
