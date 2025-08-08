5d7bdfc1ef66d5d6fb5810141cd07e073cd8bb2f
zabronax <zabronax@gmail.com>
2025-08-08 18:45:30 +0200

    setup gitops compose server

---
+++ infrastructure/app.tf
@@ -0,0 +1,33 @@
+module "compose_app" {
+  source = "./compose-app"
+
+  # Server configuration
+  server_type = "cx22" # 2 vCPU, 4GB RAM, 40GB SSD
+  location    = "hel1" # Helsinki, Finland
+
+  # Compose app configuration
+  compose_app_repo   = "https://github.com/LarsGJobloop/nix-devcontainer-example.git"
+  compose_app_branch = "gitops-introduction"
+  compose_app_path   = "compose.yaml"
+
+  # Reconciliation configuration
+  reconciliation_intervall = "1min"
+
+  # SSH Key configuration
+  # Provided here as this is for exploration and education purposes.
+  # In production, we prefer to lock down servers and disable SSH access.
+  ssh_key_id = hcloud_ssh_key.ssh_key.id
+}
+
+output "compose_app" {
+  description = "The details of the compose app"
+  value = {
+    ipv4_address = module.compose_app.server_ip
+  }
+}
+
+# Outputed for aiding with debugging
+output "cloud_config" {
+  description = "The rendered cloud-config for the server"
+  value       = module.compose_app.cloud_config
+}
